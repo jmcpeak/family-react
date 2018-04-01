06feb9a14929fff837bce40ca06ab9f19b2a57a3
jmcpeak <jason.mcpeak@gmail.com>
2018-04-01 16:56:05 -0400

    Make selected User the correct color

--- src/components/User/UserListItemWithMenu.js
+++ src/components/User/UserListItemWithMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { withStyles } from 'material-ui/styles';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
@@ -26,10 +27,29 @@ const mapStateToProps = state => ({
     hideUserMenu: hideUserMenu,
     showUserMenu: showUserMenu,
     setListMenuAnchorEl: setListMenuAnchorEl
-  };
+  },
+  styles = theme => ({
+    selectedItem: {
+      '&:focus': {
+        backgroundColor: theme.palette.primary.main,
+        '& $primary': {
+          color: theme.palette.common.white
+        },
+        '& $colorSecondary': {
+          color: theme.palette.primary.main
+        },
+        '& $colorDefault': {
+          backgroundColor: theme.palette.common.white
+        }
+      }
+    },
+    primary: {},
+    colorDefault: {},
+    colorSecondary: {}
+  });
 
 const UserListItemWithMenu = props => {
-  const { listMenuAnchorEl, position, user } = props,
+  const { classes, listMenuAnchorEl, position, user } = props,
     ICON_BUTTON_ID = `iconButtonMenu${position}`,
     mouseOut = () => props.hideUserMenu(position),
     mouseOver = () => props.showUserMenu(position),
@@ -38,14 +58,27 @@ const UserListItemWithMenu = props => {
   return (
     <ListItem
       button={true}
+      classes={{
+        button: classes.selectedItem
+      }}
+      divider={true}
       onClick={setUser}
       onMouseOver={mouseOver}
       onMouseOut={mouseOut}
     >
-      <Avatar>
-        <Icon>face</Icon>
+      <Avatar classes={{ colorDefault: classes.colorDefault }}>
+        <Icon
+          classes={{ colorSecondary: classes.colorSecondary }}
+          color={'secondary'}
+        >
+          face
+        </Icon>
       </Avatar>
-      <ListItemText primary={user.team} secondary={user.text} />
+      <ListItemText
+        classes={{ primary: classes.primary }}
+        primary={user.team}
+        secondary={user.text}
+      />
       <ListItemSecondaryAction
         style={{
           visibility: props.userMenus[position]
@@ -77,5 +110,7 @@ UserListItemWithMenu.propTypes = {
 };
 
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(UserListItemWithMenu)
+  withStyles(styles)(
+    connect(mapStateToProps, mapDispatchToProps)(UserListItemWithMenu)
+  )
 );
